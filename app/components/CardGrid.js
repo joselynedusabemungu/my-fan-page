3cc5361fe86ffbe6c39a49b4ee1e35e61eaf71b7
Joselyne Dusabemungu <joselynedusabemungu@gmail.com>
2026-06-19 17:11:12 +0300

    change made

--- app/components/CardGrid.js
+++ app/components/CardGrid.js
@@ -8,7 +8,7 @@ export default function CardGrid({ items }) {
       {items.map((item) => (
         <Card
           key={item.id}
-          name={item.name}
+          name={<Link href={"/faves/" + item.id}>{item.name}</Link>}
           blurb={item.blurb}
           rating={item.rating}
           emoji={item.emoji}
